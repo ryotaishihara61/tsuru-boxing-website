060c704d4cf46c16dc4c5979750f11c529b7baa6
Ryota Ishihara <ryota61@gmail.com>
2025-06-16 10:13:38 +0700

    新しいインスピレーション動画を追加

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
 const express = require('express');
 const path = require('path');
+// const ogs = require('open-graph-scraper'); // OGSはもう使いません
 const app = express();
 const port = 3000;
 
-// EJSをテンプレートエンジンとして設定
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// 'public' フォルダを静的ファイル（画像など）の置き場所として指定
-// ▼▼▼ この行を修正します ▼▼▼
 app.use(express.static(path.join(__dirname, 'public')));
-// ▲▲▲ この行を修正します ▲▲▲
 
-// --- データ部分は変更なし ---
+// サイトに表示するデータをここにすべて記述します
 const siteData = {
   title: "Tsuru's Boxing Chronicle",
   videos: [
     { youtubeId: 'xIXFlVPQ0kM', title: '1st-lesson, Mar 2023' },
     { youtubeId: 'C2WJHLfwbco', title: '31-May, 2025' },
     { youtubeId: 'moimgg-zsfU', title: '6-June, 2025' },
-    { youtubeId: 'tK9Y-5aim00', title: '13-June, 2025' }, 
+    { youtubeId: 'tK9Y-5aim00', title: '13-June, 2025' },
   ],
+  // ▼▼▼ inspirationLinks を手動で設定します ▼▼▼
+  inspirationLinks: [
+    {
+      title: "Best Moments Of Naoya Inoue Showing His Greatness | TOP RANK'D",
+      url: "https://www.youtube.com/watch?v=wszk2OfPv9A",
+      // YouTubeの動画ID（URLの v= の後ろ）を使ってサムネイルURLを指定
+      thumbnail: "https://i.ytimg.com/vi/wszk2OfPv9A/hqdefault.jpg", 
+      description: "We ranked the best moments in which Naoya Inoue showed his superiority in the ring."
+    },
+    {
+      title: "Junto Nakatani (26-0) Highlights & Knockouts",
+      url: "https://www.youtube.com/watch?v=wgU_XUqa0zk",
+      thumbnail: "https://i.ytimg.com/vi/wgU_XUqa0zk/hqdefault.jpg",
+      description: "Junto Nakatani Highlights & Knockouts. Best Boxing & UFC Content."
+    },
+    {
+      "title": "CRAZY KNOCKOUT POWER! Gervonta Davis HIGHLIGHTS & KNOCKOUTS",
+      "url": "https://www.youtube.com/watch?v=upqk_vErdHo",
+      "thumbnail": "https://i.ytimg.com/vi/upqk_vErdHo/hqdefault.jpg",
+      "description": "Gervonta Davis is one of the BIGGEST PUNCHERS in boxing today. Take a look at some of his best moments & knockouts inside the ring!"
+    }
+  ]
 };
 
-// --- ここから下も変更なし ---
+// トップページにアクセスが来た時の処理
 app.get('/', (req, res) => {
+  // 用意しておいたデータをそのままテンプレートに渡す
   res.render('index', siteData);
 });
 
