544f032061723311433ffbd55da65904b1f5da72
ryotaishihara61 <noreply@github.com>
2025-08-15 16:20:00 +0700

    add video

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const siteData = {
     { youtubeId: 'tK9Y-5aim00', title: '13-June, 2025' },
     { youtubeId: '89nPWWKfWlo', title: '20-June, 2025' },
     { youtubeId: 'CZbnSy9_k1o', title: '8-August, 2025' },
+    { youtubeId: 'KAhIIs8FhXE', title: '15-August, 2025' },
   ],
   // ▼▼▼ inspirationLinks を手動で設定します ▼▼▼
   inspirationLinks: [
